de9ae176449341756db82eafd2a8f74c70103bf6
Audrey Jones <audrey.eliz.jones@gmail.com>
2018-08-21 15:34:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = ["X", "X", "X", "X", "X", "X", "X", "X"]
+def display_board(board)
+  
+  puts"The first piece of data in this board is #{board[0]}."
+end
+
+display_board(board)
