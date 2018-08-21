b21a689b2484f0cbe5eb77bab41d01419219d668
Audrey Jones <audrey.eliz.jones@gmail.com>
2018-08-21 15:35:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,10 @@
 board = ["X", "X", "X", "X", "X", "X", "X", "X"]
 def display_board(board)
-  puts 
+  puts " X | X | X "
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")"
 
 display_board(board)
+
